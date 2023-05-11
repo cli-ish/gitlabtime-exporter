31d8fbb78b5b31ca1e7dcbc87683652d39ef60e6
Vincent Schneider <vincent.schneider@gmx.net>
2023-05-11 09:45:01 +0200

    added basic tool

---
+++ README.md
@@ -0,0 +1,31 @@
+# GitlabTime Exporter
+
+Export all the time you spend on a project with problems with a quick and easy-to-use Python script.
+
+### Installation and Usage
+
+```bash
+git clone https://github.com/cli-ish/gitlabtime-exporter
+cd gitlabtime-exporter
+pip3 install -r requirements.txt
+python3 export.py --gitlab-instance https://yourgitlab.com --project-name group/project --access-token xxxxx-XxXxXxXxXxXxXxXxXxXx
+cat timesheet-*.csv
+```
+
+You can obtain the access token for Gitlab at https://yourgitlab.com/-/profile/personal_access_tokens
+
+A sample output might look like the following:
+
+```csv
+Spend At,Project,Issue Id,Title,Time Spend,User,Summary
+10.02.2023,group/project,#500,Ticket 3,0:20:00,User 3,test message 123
+08.02.2023,group/project,#500,Ticket 3,1:00:00,User 2,Some example summary
+01.02.2023,group/project,#400,Ticket 2,0:30:00,User 2,
+01.02.2017,group/project,#120,Ticket 1,1:30:00,User 1,Test
+```
+
+### Note
+
+In some edge cases where you have more than 100 spend commands in an issue, there is a possibility that not all of them will be returned,
+I haven't tested this edge case yet. It is pretty unlikely to happen anyway. It could be fixed if the script takes these outputs into a list
+and retrieve them later with a paging.
---
+++ export.py
@@ -0,0 +1,112 @@
+import argparse
+import csv
+import datetime
+import json
+import time
+from datetime import datetime as datetime_format
+
+import requests
+
+
+def get_graphql_response(url, token, query):
+    data = requests.post(url, json=query, headers={"Authorization": "Bearer " + token})
+    return data.content.decode()
+
+
+def get_issue_spend_times(gitlab_url, bearer_token, project_name):
+    ql_query = """
+query getProject($path: ID!, $afterCursor: String, $pageSize: Int) {
+  project(fullPath: $path) {
+    issues(first: $pageSize, after: $afterCursor) {
+      edges {
+        node {
+          iid
+          title
+          timelogs {
+            edges {
+              node {
+                spentAt
+                user { name }
+                summary
+                timeSpent
+              }
+            }
+            totalSpentTime
+          }
+        }
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+    }
+  }
+}
+    """
+
+    issues_found = []
+    after = ""
+    while True:
+        result2 = get_graphql_response(gitlab_url + "/api/graphql", bearer_token, {
+            "operationName": "getProject",
+            "query": ql_query,
+            "variables": {
+                "pageSize": 100,
+                "path": project_name,
+                "afterCursor": after
+            }
+        })
+        result = json.loads(result2)
+
+        issue_times = result["data"]["project"]["issues"]
+        for issueNode in issue_times["edges"]:
+            node = issueNode["node"]
+            for time_node in node["timelogs"]["edges"]:
+                new_issue = {
+                    "iid": node["iid"],
+                    "title": node["title"],
+                    "date": time_node["node"]["spentAt"],
+                    "user": time_node["node"]["user"]["name"],
+                    "summary": time_node["node"]["summary"],
+                    "spend": time_node["node"]["timeSpent"],
+                }
+                issues_found.append(new_issue)
+
+        if issue_times["pageInfo"]["hasNextPage"]:
+            after = issue_times["pageInfo"]["endCursor"]
+        else:
+            break
+    return issues_found
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gitlab-instance', dest='gitlab', type=str, help='url of gitlab (https://yourgitlab.com)')
+    parser.add_argument('--project-name', dest='project', type=str, help='project path (group/project)')
+    parser.add_argument('--access-token', dest='access_token', type=str,
+                        help='user access token (xxxxx-XxXxXxXxXxXxXxXxXxXx)')
+    args = parser.parse_args()
+
+    issues = get_issue_spend_times(args.gitlab, args.access_token, args.project)
+    filename = 'timesheet-' + args.project.replace("/", "_") + "_" + time.strftime("%Y-%m-%d") + '.csv'
+    # newline='' added because windows added 2 newlines for each newline?
+    with open(filename, 'w', newline='', encoding="utf8") as file:
+        w = csv.writer(file)
+        w.writerow(["Spend At", "Spend At Clock", "Project", "Issue Id", "Title", "Time Spend", "User", "Summary"])
+        f = "%Y-%m-%dT%H:%M:%SZ"
+        for issue in issues:
+            out = datetime_format.strptime(issue["date"], f)
+            w.writerow(
+                [
+                    out.strftime("%d.%m.%Y"),
+                    out.strftime("%H:%M:%S"),
+                    args.project, "#" + issue["iid"],
+                    issue["title"],
+                    str(datetime.timedelta(seconds=issue["spend"])),
+                    issue["user"],
+                    issue["summary"]
+                ])
+
+
+if __name__ == "__main__":
+    main()
